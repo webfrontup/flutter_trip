24ecf9490ea150bc9183390d5f2ea55c46bc1b1d
yyccQQu <643929860@qq.com>
2019-05-26 21:20:14 +0700

    ninghao demo

---
+++ my_app/lib/ninghao/demo/appBar/drawer.1.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+            splashColor: Colors.white70,
+        )
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+//          leading: IconButton( //自己添加的leading
+//            icon: Icon(Icons.menu),
+//            tooltip: 'Navigration',
+//            onPressed: () => debugPrint('Navigration button is pressed.'),
+//          ),
+          title: Text('NINGHAO'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => debugPrint('Search button is pressed.'),
+            )
+          ],
+          elevation: 0.0,
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              // 有了抽屉组件以后，appbar头部会有默认的leading
+              DrawerHeader(  // 抽屉头部
+                child: Text('header'.toUpperCase()),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                ),
+              ),
+              ListTile( // 抽屉内容
+                title: Text('Message', textAlign: TextAlign.right,),
+                trailing: Icon(Icons.message, color: Colors.black12, size: 22.0,),
+                onTap: () => Navigator.pop(context), //点击关闭抽屉
+              ),
+              ListTile( // 抽屉内容
+                title: Text('Favorite', textAlign: TextAlign.right,),
+                trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0,),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile( // 抽屉内容
+                title: Text('Settings', textAlign: TextAlign.right,),
+                trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0,),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
+//        endDrawer: Text('This is drawer'),
+      ),
+    );
+  }
+}
+
