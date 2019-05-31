2ed0aca0770fa8f9e7cc1c05792ec27e4913e28c
js103 <643929860@qq.com>
2019-05-31 16:05:56 +0700

    jspang 06

---
+++ my_app/lib/jspang_shop/pages/member_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MemberPage extends StatefulWidget {
+  @override
+  _MemberPageState createState() => _MemberPageState();
+}
+
+class _MemberPageState extends State<MemberPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
