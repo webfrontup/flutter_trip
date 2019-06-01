ead81877ac581224bc31a37653d877fd7bda4cca
js103 <643929860@qq.com>
2019-06-01 09:21:13 +0700

    easy-mock post

--- my_app/lib/jspang_shop/pages/home_page.dart
+++ my_app/lib/jspang_shop/pages/home_page.dart
@@ -13,35 +13,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-
       child: Scaffold(
           appBar: AppBar(title: Text('美好人间'),),
-          body:Container(
-            height: 1000,
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  controller:typeController,
-                  decoration:InputDecoration (
-                      contentPadding: EdgeInsets.all(10.0),
-                      labelText: '美女类型',
-                      helperText: '请输入你喜欢的类型'
-                  ),
-                  autofocus: false,
+          // SingleChildScrollView: 一个可以滚动的widget框 没有组件实体
+          body:SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller:typeController,
+                      decoration:InputDecoration (
+                          contentPadding: EdgeInsets.all(10.0),
+                          labelText: '美女类型',
+                          helperText: '请输入你喜欢的类型'
+                      ),
+                      autofocus: false,
+                    ),
+                    RaisedButton(
+                      onPressed:_choiceAction,
+                      child: Text('选择完毕'),
+                    ),
+                    Text(
+                      showText,
+                      overflow:TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ),
+                  ],
                 ),
-                RaisedButton(
-                  onPressed:_choiceAction,
-                  child: Text('选择完毕'),
-                ),
-
-                Text(
-                  showText,
-                  overflow:TextOverflow.ellipsis,
-                  maxLines: 2,
-                ),
-
-              ],
-            ),
+              )
           )
       ),
     );
@@ -65,14 +64,20 @@ class _HomePageState extends State<HomePage> {
 
   }
 
+  // https://time.geekbang.org/serv/v1/column/newAll
+
   Future getHttp(String TypeText)async{
     try{
       Response response;
       var data={'name':TypeText};
-      response = await Dio().get(
-          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
+//      response = await Dio().get(
+//          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
+//          queryParameters:data
+//      );
+        response = await Dio().post(
+          "https://easy-mock.com/mock/5cf1dad6e0e1875766c67ed8/example/dabaojian_post",
           queryParameters:data
-      );
+        );
       return response.data;
     }catch(e){
       return print(e);
