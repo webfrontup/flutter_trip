7534ceb462ac24444a64f9b471bb670696f60ec4
yyccQQu <643929860@qq.com>
2019-06-03 22:55:55 +0700

    首页导航 拨打电话操作

--- my_app/lib/jspang_shop/pages/home_page.dart
+++ my_app/lib/jspang_shop/pages/home_page.dart
@@ -3,6 +3,8 @@ import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 
 class HomePage extends StatefulWidget {
@@ -37,9 +39,17 @@ class _HomePageState extends State<HomePage> {
               var data=json.decode(snapshot.data.toString());
               print(data);
               List<Map> swiperDataList = (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+              List<Map> navigatorList = (data['data']['category'] as List).cast();
+              String advertesPicture = data['data']['advertesPicture']['PICTURE_ADDRESS']; //广告图片
+              String  leaderImage= data['data']['shopInfo']['leaderImage'];  //店长图片
+              String  leaderPhone = data['data']['shopInfo']['leaderPhone']; //店长电话
+
               return Column(
                 children: <Widget>[
                   SwiperDiy(swiperDataList:swiperDataList ),   //页面顶部轮播组件
+                  TopNavigator(navigatorList: navigatorList),
+                  AdBanner(advertesPicture:advertesPicture),   //广告组件
+                  LeaderPhone(leaderImage:leaderImage,leaderPhone: leaderPhone)  //广告组件
                 ],
               );
             }else{
@@ -78,5 +88,84 @@ class SwiperDiy extends StatelessWidget {
         autoplay: true,
       ),
     );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context,item){
+    return InkWell(
+      onTap: (){print('点击了导航');},
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'],width:ScreenUtil().setWidth(95)),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(this.navigatorList.length>10){
+      this.navigatorList.removeRange(10, this.navigatorList.length);
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding:EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+//广告图片
+class AdBanner extends StatelessWidget {
+  final String advertesPicture;
+
+  AdBanner({Key key, this.advertesPicture}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Container(
+      child: Image.network(advertesPicture),
+    );
+  }
+}
+
+// 编写店长电话模块
+class LeaderPhone extends StatelessWidget {
+  final String leaderImage; //店长图片
+  final String leaderPhone; //店长电话
+
+  LeaderPhone({Key key, this.leaderImage,this.leaderPhone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap:_launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _launchURL() async {
+//    String url = 'tel:'+leaderPhone;
+    String url = 'http://flutter.io';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
